869de6dee35afe9690a39909a0edeacfaefe75be
S.U <s.uglovyi@gmail.com>
2021-09-17 00:33:18 +0300

    complete day5 tasks

--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -11,27 +11,3 @@ public class Task2 {
 
     }
 }
-class Motorbike {
-    private String model;
-    private String color;
-    private int year;
-
-    public Motorbike(String model, String color, int year) {
-        this.model = model;
-        this.color = color;
-        this.year = year;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-}
