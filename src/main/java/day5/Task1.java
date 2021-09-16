2eff3b6eb838c5d052901df40ac68b91a78ddce1
S.U <s.uglovyi@gmail.com>
2021-09-16 22:28:08 +0300

    complete day5 tasks

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,43 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setColor("green");
+        car.setYear(1978);
+        car.setModel("zaz");
+        System.out.println("Color is " + car.getColor());
+        System.out.println("Model is " + car.getModel());
+        System.out.println("Year is " + car.getYear());
+    }
+}
 
+class Car {
+    private String model;
+    private String color;
+    private int year;
+
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,35 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike car = new Motorbike("green", "zaz", 1978);
+
+        System.out.println("Color is " + car.getColor());
+        System.out.println("Model is " + car.getModel());
+        System.out.println("Year is " + car.getYear());
 
     }
+}
+class Motorbike {
+    private String model;
+    private String color;
+    private int year;
+
+    public Motorbike(String model, String color, int year) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
 }
