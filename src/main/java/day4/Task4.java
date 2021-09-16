7f5b4f6521a0166bb94cb54b55ff394eee187f4b
S.U <s.uglovyi@gmail.com>
2021-09-17 00:34:50 +0300

    complete day5 tasks

--- src/main/java/day4/Task4.java
+++ src/main/java/day4/Task4.java
@@ -1,7 +1,15 @@
 package day4;
 
+import java.util.Random;
+
 public class Task4 {
     public static void main(String[] args) {
+        Random rand = new Random();
+
+        int[] arr = new int[100];
+        for (int i = 0; i < 100; i++) {
+            arr[i] = rand.nextInt(10000);
+        }
 
     }
 }
