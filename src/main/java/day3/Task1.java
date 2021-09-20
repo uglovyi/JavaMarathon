b8558729f4465159ca5750a20dc7063e3f217c57
S.U <s.uglovyi@gmail.com>
2021-09-20 12:12:37 +0300

    complete day3 tasks

--- src/main/java/day3/Task1.java
+++ src/main/java/day3/Task1.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scam = new Scanner(System.in);
         while(true) {
-            Scanner scam = new Scanner(System.in);
             String str = scam.next();
             if(str.equals("Stop")) {
                 break;
--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         while(true){
-            Scanner scanner = new Scanner(System.in);
             double num1 = scanner.nextDouble();
             double num2 = scanner.nextDouble();
             if(num2 == 0){
