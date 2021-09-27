8a9bd6fd667ae127274d8b375127f8753ca81e1f
S.U <s.uglovyi@gmail.com>
2021-09-27 23:34:46 +0300

    complete day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,34 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("shoes.csv");
+        try {
+            Scanner scanner = new Scanner(file);
+            List<String> list = new ArrayList<>();
+
+            while (scanner.hasNext()){
+                String[] dim = scanner.nextLine().split(";");
+                if("0".equals(dim[2])){
+                    //Вариант 1
+                    list.add((new StringBuilder()).append(dim[0]).append(", ").append(dim[1]).append(", ").append(dim[2]).toString());
+                    //Вариант 2
+                    list.add(String.format("%s , %s, %s", dim[0], dim[1], dim[2]));
+                }
+            }
+            for (String str:
+                 list) {
+                System.out.println(str);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
     }
 }
